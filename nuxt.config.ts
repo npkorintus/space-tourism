3dbebd59679194c7fd188381d153c38c4f25c594
Nick Korintus <nicholas.korintus@gmail.com>
2025-03-05 14:12:58 -0500

    adding google fonts to project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+
+  devServer: {
+    host: '0.0.0.0',
+    port: 3000,
+  },
+
+  css: [
+    '@/assets/styles/main.scss'
+  ],
+
+  modules: ['@nuxtjs/google-fonts'],
+  googleFonts: {
+    families: {
+      'Bellefair': true,
+      'Barlow Condensed': true,
+      'Barlow': true
+    }
+  }
 })
