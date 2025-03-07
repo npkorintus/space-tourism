40f777a15d4bf85fb8592abd2942a62e45bc8bc0
Nick Korintus <nicholas.korintus@gmail.com>
2025-03-06 22:23:42 -0500

    adding content to Home page, updating default layout, adding stylesheets

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,7 @@
 <template>
-  <header></header>
-  <main>
-    <slot></slot>
+  <a class="skip-to-content" href="#main">Skip to content</a>
+  <Header />
+  <main id="main">
+    <slot />
   </main>
 </template>
